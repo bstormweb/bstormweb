8c767bd88973513ddd6918996f4e3e3ac0f44580
Joel Davis <joeld42@gmail.com>
2025-08-05 21:45:00 -0700

    Setting up skeleton structure

---
+++ content/wheelset/Character-Builder.md
@@ -0,0 +1,6 @@
+---
+title: Character Builder
+description: Create characters
+bstormXML: /uploads/Character Builder.xml
+---
+
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './content/**/*.{html,js,md}',
+    './layouts/**/*.{html,js}',
+    './themes/**/*.{html,js}',
+    './hugo_stats.json'
+  ],
+  plugins: [],
+}
